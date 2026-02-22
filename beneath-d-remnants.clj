5e4b86a693fcdd6ace497938856223f59f251cf0
vieirandre <andre.vieira@outlook.com>
2026-02-22 18:20:01 -0300

    Print init cleanup target

--- beneath-d-remnants.clj
+++ beneath-d-remnants.clj
@@ -1,1 +1,12 @@
 #!/usr/bin/env bb
+
+(ns beneath-d-remnants
+  (:require [clojure.string :as str]))
+  
+(defn normalize [s]
+  (str/lower-case (str/trim s)))
+
+(def target "MSI Afterburner")
+
+(println "beneath-d-remnants")
+(println "Target:" (normalize target))
