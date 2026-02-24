05cc05c8256aa34fd48c45d705ea016118a57ea1
vieirandre <andre.vieira@outlook.com>
2026-02-24 05:15:26 -0300

    Add windows check

--- beneath_d_remnants.clj
+++ beneath_d_remnants.clj
@@ -2,11 +2,19 @@
 
 (ns beneath-d-remnants
   (:require [clojure.string :as str]))
-  
+
 (defn normalize [s]
   (str/lower-case (str/trim s)))
 
+(defn windows?
+  []
+  (str/includes? (normalize (System/getProperty "os.name")) "windows"))
+
 (def target "MSI Afterburner")
 
-(println "beneath-d-remnants")
+(when-not (windows?)
+  (binding [*out* *err*]
+    (println "this scrpt is intended for windows"))
+  (System/exit 1))
+
 (println "Target:" (normalize target))
